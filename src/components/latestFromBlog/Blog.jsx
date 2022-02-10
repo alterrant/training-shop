7dc7430ab327a44601815ae0de39116ee0344de0
alterrant <pandor246@mail.ru>
2022-02-10 14:25:38 +0300

    fix: fix named alt

--- src/components/latestFromBlog/Blog.jsx
+++ src/components/latestFromBlog/Blog.jsx
@@ -4,7 +4,7 @@ const Blog = ({item}) => {
 
   return (
       <li className={BlogStyle.wrapper}>
-        <img className={BlogStyle.img} src={item.img} alt="item.image"/>
+        <img className={BlogStyle.img} src={item.img} alt={item.image}/>
         <div className={BlogStyle.post}>
           <h3 className={BlogStyle.tittle}>{item.tittle}</h3>
           <p className={BlogStyle.description}>{item.description}</p>
