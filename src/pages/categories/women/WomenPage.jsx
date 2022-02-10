51a41212639c074ebdd61e98addac33f78149d73
alterrant <pandor246@mail.ru>
2022-02-10 14:13:49 +0300

    add: women and empty womenId pages

---
+++ src/pages/categories/women/WomenPage.jsx
@@ -0,0 +1,11 @@
+import WomenS from "../../../components/women's/Women's";
+
+const WomenPage = () => {
+  return (
+      <div>
+        <WomenS/>
+      </div>
+  )
+}
+
+export default WomenPage;
---
+++ src/pages/product/WomenId.jsx
@@ -0,0 +1,9 @@
+const WomenId = () => {
+  return (
+      <div>
+        WomenId
+      </div>
+  )
+}
+
+export default WomenId;
