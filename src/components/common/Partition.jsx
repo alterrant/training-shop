bcaeb7bfbfe1d8d3035f77c349e5c371bdfcb286
alterrant <pandor246@mail.ru>
2022-02-10 14:02:36 +0300

    add: add rating and underscore blocks

---
+++ src/components/common/Partition.jsx
@@ -0,0 +1,10 @@
+import PartitionStyle from './Partition.module.css';
+
+const Partition = () => {
+  return (
+      <div className={PartitionStyle.underline}>
+      </div>
+  )
+}
+
+export default Partition;
