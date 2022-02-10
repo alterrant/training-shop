3d4278dc4431bd077082c9598f84087e1a1b2297
alterrant <pandor246@mail.ru>
2022-02-10 14:03:38 +0300

    add: latestFromBlog, blogs

---
+++ src/components/latestFromBlog/LatestFromBlog.jsx
@@ -0,0 +1,29 @@
+import LatestFromBlogStyle from "./LatestFromBlog.module.css";
+import {BLOGS} from "../../constants/blog";
+import Blog from "./Blog";
+
+const LatestFromBlog = () => {
+
+  const blogList = BLOGS.map(item =>
+      <Blog key={item.id} item={item}/>
+  )
+
+  return (
+      <section className={LatestFromBlogStyle.wrapper}>
+        <div className={LatestFromBlogStyle.header}>
+          <h4 className={LatestFromBlogStyle.tittle}>
+            LATEST FROM BLOG
+          </h4>
+          <div className={LatestFromBlogStyle.seeAllButton}>
+            SEE ALL
+          </div>
+        </div>
+        <ul className={LatestFromBlogStyle.blogs}>
+          {blogList}
+        </ul>
+      </section>
+
+  )
+}
+
+export default LatestFromBlog;
