915f261cc0fee7d7d51cb17c509bd578c9d8d0dc
alterrant <pandor246@mail.ru>
2022-02-11 14:08:16 +0300

    add: tittle, filter, preloader to WomenPage

--- src/pages/categories/women/WomenPage.jsx
+++ src/pages/categories/women/WomenPage.jsx
@@ -1,10 +1,24 @@
 import WomenS from "../../../components/women's/Women's";
+import Tittle from "../../../components/tittle/Tittle";
+import Preloader from "../../../components/common/Preloader";
+import {useState} from "react";
+import ProductFilter from "../../../components/filter/ProductFilter";
+import FilterCategories from "../../../components/filter/FilterCategories";
 
 const WomenPage = () => {
+
+  const [isLoading] = useState(true);
+  const [isOpenedFilter, setOpenedStatusFilter] = useState(false);
+
   return (
-      <div>
+      <article>
+        <Tittle tittle={"WOMEN"}/>
+        <ProductFilter isOpenedFilter={isOpenedFilter}
+                       setOpenedStatusFilter={setOpenedStatusFilter}/>
+        {isOpenedFilter && <FilterCategories/>}
         <WomenS/>
-      </div>
+        {isLoading && <Preloader/>}
+      </article>
   )
 }
 
