0e87dffd9c884541fc741e278dd07e7c64d9b4ba
alterrant <pandor246@mail.ru>
2022-02-11 14:09:37 +0300

    add: tittle to womenId page

--- src/pages/product/WomenId.jsx
+++ src/pages/product/WomenId.jsx
@@ -1,6 +1,12 @@
+import Tittle from "../../components/tittle/Tittle";
+import {useParams} from "react-router-dom";
+
 const WomenId = () => {
+
+  const {id} = useParams();
   return (
       <div>
+        <Tittle tittle={id}/>
         WomenId
       </div>
   )
