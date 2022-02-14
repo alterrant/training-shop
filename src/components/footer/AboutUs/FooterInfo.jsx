f95ce95742b4df352ace99f1bb6e8ad1eb8426ea
alterrant <pandor246@mail.ru>
2022-02-14 19:56:58 +0300

    add: footerLinks

--- src/components/footer/AboutUs/FooterInfo.jsx
+++ src/components/footer/AboutUs/FooterInfo.jsx
@@ -1,16 +1,18 @@
 import FooterInfoStyle from "./FooterInfo.module.css";
+import {Link} from "react-router-dom";
 
-const FooterInfo = (props) => {
-  const list = props.children.map((item, index) =>
-      <li key={index}>
-        {item}
+const FooterInfo = ({links, tittle}) => {
+  const list = links.map((item) =>
+      <li key={item.id}>
+        <Link className={FooterInfoStyle.link} to={`${item.path}`}>{item.name}</Link>
       </li>
   );
 
   return (
       <li className={FooterInfoStyle.list}>
+        <h4 className={FooterInfoStyle.title}>{tittle}</h4>
         <ul>
-        {list}
+          {list}
         </ul>
       </li>
   )
