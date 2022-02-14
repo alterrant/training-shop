e19caafddf2cf52faf721ecb9a819cdf84d716b5
alterrant <pandor246@mail.ru>
2022-02-14 19:58:09 +0300

    refactor: footer with links

--- src/components/footer/AboutUs/AboutUs.jsx
+++ src/components/footer/AboutUs/AboutUs.jsx
@@ -1,39 +1,23 @@
 import AboutUsStyle from "./AboutUs.module.css";
 import FooterInfo from "./FooterInfo";
-import {CompanyAddress, CompanyMail, CompanyPhone, CompanySchedule} from "../../common/CompanyInfo";
+import {FOOTERNAVBAR} from "../../../constants/menuConst";
+import {getNavLinks} from "../../../encapsulatedCommonLogics/getNavLinks";
 
 const AboutUs = () => {
+
+  const footerLinksLists = FOOTERNAVBAR.map((item) => {
+        const {tittle, description} = getNavLinks(item);
+
+        return <FooterInfo key={tittle}
+                           links={description}
+                           tittle={tittle}/>
+      }
+  )
+
   return (
       <div className={AboutUsStyle.container}>
         <ul className={AboutUsStyle.wrapper}>
-          <FooterInfo>
-            <h4 className={AboutUsStyle.title}>CATEGORIES</h4>
-            <p>Men</p>
-            <p>Women</p>
-            <p>Accessories</p>
-            <p>Beauty</p>
-          </FooterInfo>
-          <FooterInfo>
-            <h4 className={AboutUsStyle.title}>INFORMATION</h4>
-            <p>About Us</p>
-            <p>Contact Us</p>
-            <p>Blog</p>
-            <p>FAQs</p>
-          </FooterInfo>
-          <FooterInfo>
-            <h4 className={AboutUsStyle.title}>USEFUL LINKS</h4>
-            <p>Terms & Conditions</p>
-            <p>Returns & Exchanges</p>
-            <p>Shipping & Delivery</p>
-            <p>Privacy Policy</p>
-          </FooterInfo>
-          <FooterInfo>
-            <h4 className={AboutUsStyle.title}>CONTACT US</h4>
-            <CompanyAddress/>
-            <CompanyPhone/>
-            <CompanySchedule/>
-            <CompanyMail/>
-          </FooterInfo>
+          {footerLinksLists}
         </ul>
       </div>
   )
---
+++ src/encapsulatedCommonLogics/getNavLinks.js
@@ -0,0 +1,7 @@
+export const getNavLinks = (item) => {
+
+  const tittle = Object.keys(item)[0];
+  const description = item[tittle];
+
+  return {tittle, description}
+}
