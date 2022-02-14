8f73445cda9cbe20dd8b40287ea1a5c923ffd1b9
alterrant <pandor246@mail.ru>
2022-02-14 20:37:20 +0300

    add: class and data-test-id to App

--- src/pages/app/App.jsx
+++ src/pages/app/App.jsx
@@ -1,14 +1,15 @@
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
+import AppStyle from "./App.module.css";
 import {Outlet} from "react-router-dom";
 
 const App = () => {
   return (
-      <>
+      <div className={AppStyle.app} data-test-id='app'>
         <Header/>
         <Outlet />
         <Footer/>
-      </>
+      </div>
   )
 }
 
