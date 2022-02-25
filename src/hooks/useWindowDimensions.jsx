ecc1f4c12453d65362e6c9b4dc9a4cc2c1c21e6b
alterrant <pandor246@mail.ru>
2022-02-25 17:33:26 +0300

    delete: unused hook

--- src/hooks/useWindowDimensions.jsx
+++ src/hooks/useWindowDimensions.jsx
@@ -1,35 +1,6 @@
 import { useState, useEffect } from 'react';
 import {getWindowDimensions} from "../encapsulatedCommonLogics/getWindowDimensions";
 
-/*export function useWindowDimensions1() {
-
-  const hasWindow = typeof window !== 'undefined';
-
-  function getWindowDimensions() {
-    const width = hasWindow ? window.innerWidth : null;
-    const height = hasWindow ? window.innerHeight : null;
-    return {
-      width,
-      height,
-    };
-  }
-
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-  useEffect(() => {
-    if (hasWindow) {
-      function handleResize() {
-        setWindowDimensions(getWindowDimensions());
-      }
-
-      window.addEventListener('resize', handleResize);
-      return () => window.removeEventListener('resize', handleResize);
-    }
-  }, [windowDimensions]);
-
-  return windowDimensions;
-}*/
-
 export default function useWindowDimensions() {
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
