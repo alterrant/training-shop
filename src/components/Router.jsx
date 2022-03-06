a3554973680942bc8a793e7b1a445269e89e756e
alterrant <pandor246@mail.ru>
2022-03-07 00:37:20 +0300

    fix: dispatch dependency

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -6,14 +6,14 @@ import ProductsPage from "../pages/categories/ProductsPage";
 import {useDispatch, useSelector} from "react-redux";
 import {useCallback, useEffect} from "react";
 import {setInitSuccess} from "../redux/initializeReducer";
-// import {useStableDispatch} from "../hooks/useRedux";
+import {useStableDispatch} from "../hooks/useRedux";
 
 export const Router = () => {
 
   const isInit = useSelector((state) => state.initialize.isInit);
-  // const dispatch = useStableDispatch();
-  const dispatch = useDispatch();
-  const stableDispatch = useCallback(dispatch, [dispatch]);
+  const stableDispatch = useStableDispatch();
+  // const dispatch = useDispatch();
+  // const stableDispatch = useCallback(dispatch, [dispatch]);
 
   useEffect(() => {
     //потом будет логика инициализации
