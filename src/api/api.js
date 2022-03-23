f21475737a40310ffbbf99776bb6fd3be1511ae2
alterrant <pandor246@mail.ru>
2022-03-23 18:16:20 +0300

    refactor: comment axios get timeout

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: 'https://training.cleverland.by/shop',
-  timeout: 1000,
+  // timeout: 1000,
   //headers, withCredentials пока не нужно
 })
