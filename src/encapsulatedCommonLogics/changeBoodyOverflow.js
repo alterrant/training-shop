017efbc8f13520d829891f4e62232d7ce588bfcd
alterrant <pandor246@mail.ru>
2022-04-05 17:01:20 +0300

    fix: fix width jumps

--- src/encapsulatedCommonLogics/changeBoodyOverflow.js
+++ src/encapsulatedCommonLogics/changeBoodyOverflow.js
@@ -1,8 +1,19 @@
 export const changeBodyOverflow = (isMenuOpen) => {
 
   if (!isMenuOpen) {
+    const windowWidth = document.documentElement.clientWidth;
+
     document.body.style.overflow = 'hidden';
+
+    const changedWindowWidth = document.documentElement.clientWidth;
+
+    if (changedWindowWidth !== windowWidth) {
+      document.body.style.paddingRight = (changedWindowWidth - windowWidth) + 'px';
+    }
+
   } else {
     document.body.style.overflow = 'visible';
+
+    document.body.style.paddingRight = '';
   }
 }
