2831cce165b597f5f94eec7414f768ce7fb00867
alterrant <pandor246@mail.ru>
2022-04-16 22:20:19 +0300

    add: add handleClick Back to shopping

--- src/components/shoppingCart/content/footer/ShoppingCartFooter.jsx
+++ src/components/shoppingCart/content/footer/ShoppingCartFooter.jsx
@@ -4,7 +4,7 @@ import {
   resetDeliveryFormToggle,
   resetDeliveryInfo,
   resetPaymentFormToggle,
-  resetPaymentInfo,
+  resetPaymentInfo, resetProducts,
   resetSubmission,
   setDeliveryInfo,
   setPaymentInfo, shoppingCartToggle
@@ -30,6 +30,16 @@ const ShoppingCartFooter = (props) => {
       //setNavigationStage('Payment');
     }
     if (finalShoppingCardPageName === 'emptyOrder') dispatch(shoppingCartToggle());
+
+    if (finalShoppingCardPageName === 'submittingSuccess') {
+      dispatch(resetProducts());
+      dispatch(resetDeliveryInfo());
+      dispatch(resetPaymentInfo());
+      dispatch(resetDeliveryFormToggle());
+      dispatch(resetPaymentFormToggle());
+
+      dispatch(shoppingCartToggle());
+    }
 
     if (navigationStage === 'Item in Cart') {
       setNavigationStage('Delivery Info');
